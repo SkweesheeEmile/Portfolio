581d1543581e24e430dfb825dee7ea617c380de4
SkweesheeEmile <emilev06@gmail.com>
2025-11-26 13:40:47 -0500

    started adding things

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,18 +1,16 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rjshkhr.github.io/cleanfolio',
-  title: 'JS.',
+  homepage: 'https://skweesheeemile.github.io/Portfolio/',
+  title: 'Portfolio',
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
-  picture: 'https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png',
+  name: 'Émile Lévesque',
+  role: 'Software Developper',
 
   description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
-  resume: 'https://example.com',
+    '',
   social: {
     linkedin: 'https://linkedin.com',
     github: 'https://github.com',
@@ -23,21 +21,19 @@ const projects = [
   // projects can be added an removed
   // if there are no projects, Projects section won't show up
   {
-    name: 'Project 1',
+    name: 'EnviroAction',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
-    image: 'cleanfolio.png',
+      'A one-stop shop to start taking actions towards improving the condition of our environment! Using the power of the Gemini API and the Google Maps API, you can give your daily habits and will get a custom list of actions you are able to get to start your eco friendly habits!',
+    stack: ['Gemini', 'TypeScript', 'React', "Places API"],
+    sourceCode: 'https://github.com/SkweesheeEmile/EnviroAction',
+    image: 'EnviroActions.png',
   },
   {
-    name: 'Project 2',
+    name: 'StockUp',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'StockUp is a mobile application designed to help users easily manage and track their stock investments. Its purpose is to provide a simple interface where users can view their portfolio, monitor performance, and stay organized without complex tools or spreadsheets.',
+    stack: ['Kotlin', 'Compose', 'Android'],
+    sourceCode: 'https://github.com/AveryPBerger/StockUp',
     image: 'https://github.githubassets.com/assets/GitHub-Logo-ee398b662d42.png',
   },
   {
@@ -58,18 +54,20 @@ const skills = [
   'JavaScript',
   'TypeScript',
   'React',
-  'Redux',
-  'SASS',
+  "Kotlin and Compose", 
   'Material UI',
-  'Git',
+  'Git / DevOps',
   'CI/CD',
   'Jest',
-  'Enzyme',
+  "C#",
+  "Python",
+  "Java",
+  "MERN"
 ]
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'emilev06@gmail.com',
 }
 
 export { header, about, projects, skills, contact }
